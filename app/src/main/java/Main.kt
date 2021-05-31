26905c4b534900939d79735fd335721960ff7955
grizz03 <sawilson1110@yahoo.com>
2021-05-30 23:27:12 -0700

    first commit

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,4 +1,15 @@
+import kotlin.reflect.typeOf
+
 fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
+
+    // val steve: String = "Hello, my name is Steve!"
+    val steve = 5
+    println(steve)
+
+    val steveSalary = 500
+    val monthly = steveSalary * 5
+    println(steveSalary)
+    println(monthly)
 }
