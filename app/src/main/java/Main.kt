ff989fcccfb219642a1ced92f9479d332f14dae9
grizz03 <sawilson1110@yahoo.com>
2021-05-31 12:20:07 -0700

    when statement and user input

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,9 +1,7 @@
-import kotlin.reflect.typeOf
-
 fun main(args: Array<String>) {
-    println("Hello World")
+    println("Hello Kotlin")
     println("My first Kotlin program")
-
+    // -> val is immutable
     // val steve: String = "Hello, my name is Steve!"
     val steve = 5
     println(steve)
@@ -12,4 +10,16 @@ fun main(args: Array<String>) {
     val monthly = steveSalary * 5
     println(steveSalary)
     println(monthly)
+
+    // User input through terminal
+    println("How old are you?")
+    val age = readLine()!!.toInt()
+
+    // When statement (if/else)
+    val message = when {
+        age < 18 -> "Sorry you cant vote"
+        age == 100 -> "Congrats"
+        else -> "You can vote!"
+    }
+    println(message)
 }
