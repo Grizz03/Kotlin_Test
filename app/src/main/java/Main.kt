05886fffa42221bd6ff88ef7a35093e91befc463
grizz03 <sawilson1110@yahoo.com>
2021-05-31 15:07:22 -0700

    dot notation to call class

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,28 +1,38 @@
 fun main(args: Array<String>) {
-    println("Hello Kotlin")
-    println("My first Kotlin program")
-    // -> val is immutable
+    // Dot notation to call class and show function to print info
+    val steve = Player("Steve", 3, 5, 2)
+    val kassie = Player("Kassie", 8, 11, 1)
+    steve.show()
+    kassie.show()
+}
+
+
+
+    // -> VAL IS IMMUTABLE
     // val steve: String = "Hello, my name is Steve!"
-    val steve = 5
-    println(steve)
-
-    val steveSalary = 500
-    val monthly = steveSalary * 5
-    println(steveSalary)
-    println(monthly)
-
+//    val steve = 5
+//    println(steve)
+//
+//
+//    val steveSalary = 500
+//    val monthly = steveSalary * 5
+//
+//    println(steveSalary)
+//    println(monthly)
 
 
     // User input through terminal
-    println("How old are you?")
-    val age = readLine()!!.toInt()
-    println("You entered $age")
+//    println("How old are you?")
+//    val age = readLine()!!.toInt()
 
-    // When statement (if/else)
-    val message: String = when {
-        age < 18 -> "Cant vote"
-        age == 100 -> "Congrats"
-        else -> "You can vote!"
-    }
-    println(message)
-}
+    // Print users age
+//    println("You entered $age")
+
+    // When statement (similar to if/else)
+//    val message: String = when {
+//        age < 18 -> "Sorry cant vote"
+//        age == 100 -> "Congrats"
+//        else -> "You can vote!!"
+//    }
+//    println(message)
+
